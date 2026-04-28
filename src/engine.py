b102a43202beae8d2bc1503c2a12e2813658dbfa
Kylian Mathurin <96012700+kylianmthr@users.noreply.github.com>
2026-04-28 16:14:17 +0200

    Feature: Initialize pygame

---
+++ src/engine.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Engine:
+    def __init__(self):
+        self.running = True
+        self.frame_rate = 60
+        self.screen_width = 800
+        self.screen_height = 600
+        self.screen = pygame.display.set_mode(
+            (self.screen_width, self.screen_height)
+        )
+        self.clock = pygame.time.Clock()
+
+    def run(self) -> None:
+        pygame.init()
+        while self.running:
+            self.clock.tick(self.frame_rate)
+        pygame.quit()
