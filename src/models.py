17f786b7294f21500ca6cacc0ff9c58960bdd6b2
Kylian Mathurin <96012700+kylianmthr@users.noreply.github.com>
2026-04-28 11:47:49 +0200

    Feature: Add pydantic models

---
+++ src/models.py
@@ -0,0 +1,17 @@
+from typing import TypedDict
+from pydantic import BaseModel, Field
+
+
+class LevelType(TypedDict):
+    width: int
+    height: int
+
+
+class Config(BaseModel):
+    highscore_filename: str
+    level: list[LevelType]
+    lives: int = Field(ge=1)
+    pacgum: int = Field(ge=0)
+    points_per_pacgum: int = Field(ge=0)
+    points_per_super_pacgum: int = Field(ge=0)
+    points_per_ghost: int = Field(ge=0)
