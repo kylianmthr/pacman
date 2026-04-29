9354bcfa01999ae70ad60f08406ce1005450514c
Kylian Mathurin <96012700+kylianmthr@users.noreply.github.com>
2026-04-29 13:34:36 +0200

    Feature: Add wall class and wall group

--- src/engine.py
+++ src/engine.py
@@ -1,19 +1,64 @@
 import pygame
 
+from src.wall import Wall
+from mazegen import generator
+
 
 class Engine:
-    def __init__(self) -> None:
+    def __init__(self, width: int, height: int, seed: int) -> None:
         self.running = True
         self.frame_rate = 60
         self.screen_width = 800
         self.screen_height = 600
+        self.width = width
+        self.height = height
         self.screen = pygame.display.set_mode(
             (self.screen_width, self.screen_height)
         )
         self.clock = pygame.time.Clock()
+        self.walls = pygame.sprite.Group()
+        self.maze = generator.MazeGenerator(
+            self.width,
+            self.height,
+            seed,
+            (0, 0),
+            (self.width - 1, self.height - 1),
+        )
+        self.maze.generate((0, 0))
+        self.maze.dig()
+
+    def create_walls(self) -> None:
+        offset_y = 0
+        for row in self.maze.maze:
+            offset_x = 0
+            for col in row:
+                walls = []
+                if int(col[3]):
+                    walls += [Wall(50, 10, (0 + offset_x, 0 + offset_y))]
+                if int(col[0]):
+                    walls += [Wall(10, 50, (0 + offset_x, 0 + offset_y))]
+                if col == "1111":
+                    walls += [
+                        Wall(30, 30, (10 + offset_x, 10 + offset_y), "blue")
+                    ]
+                self.walls.add(walls)
+                offset_x += 40
+            offset_y += 40
+        bottom_wall = Wall(40 * self.width + 10, 10, (0, offset_y))
+        self.walls.add(bottom_wall)
+        right_wall = Wall(10, 40 * self.height + 10, (40 * self.width, 0))
+        self.walls.add(right_wall)
+
+    def event(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
     def run(self) -> None:
         pygame.init()
+        self.walls.draw(self.screen)
+        pygame.display.flip()
         while self.running:
+            self.event()
             self.clock.tick(self.frame_rate)
         pygame.quit()
---
+++ src/wall.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        coordinates: tuple[int, int],
+        color: str = "white",
+    ) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((width, height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        x, y = coordinates
+        self.rect.x = x
+        self.rect.y = y
