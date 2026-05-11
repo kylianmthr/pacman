9ebbab44395de3bf764adf1f45c19f6a37a19e5e
Kylian Mathurin <96012700+kylianmthr@users.noreply.github.com>
2026-05-11 11:20:06 +0200

    Fix: Error handling

--- pac-man.py
+++ pac-man.py
@@ -2,10 +2,16 @@ from src.engine import Engine
 from src.parser import Parser
 
 if __name__ == "__main__":
-    parser = Parser("config.json")
-    parser.load()
-    data = parser.parse()
-    # maze = generator.MazeGenerator(5, 5, 42, (0, 0), (4, 4))
-    # maze.dig()
-    engine = Engine(data.level, data.seed)
-    engine.run()
+    try:
+        parser = Parser("config.json")
+        parser.load()
+        data = parser.parse()
+        try:
+            # maze = generator.MazeGenerator(5, 5, 42, (0, 0), (4, 4))
+            # maze.dig()
+            engine = Engine(data.level, data.seed)
+            engine.run()
+        except Exception as e:
+            print(f"Error running the game: {e}")
+    except Exception as e:
+        print(f"Error loading configuration: {e}")
