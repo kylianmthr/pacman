20f6b4728965697601bb84ea245e337b0e673e14
Kylian Mathurin <96012700+kylianmthr@users.noreply.github.com>
2026-05-11 13:05:41 +0200

    Feature: Timer limit

--- src/game.py
+++ src/game.py
@@ -1,5 +1,6 @@
 import pygame
 from src.pacgum import Pacgum
+from src.timer import Timer
 from src.wall import Wall
 from mazegen import generator
 import random
@@ -18,6 +19,7 @@ class Game:
 
         self.engine = engine
         self.lives = 3
+        self.timer = Timer(90)
         self.hud = HUD(self, self.engine)
         self.width = width
         self.height = height
@@ -62,6 +64,7 @@ class Game:
         self.sprites.add(self.orange_ghost)
         self.sprites.add(self.hud.score)
         self.sprites.add(self.hud.lives)
+        self.sprites.add(self.hud.timer)
         self.create_walls()
         self.create_pacgums()
 
@@ -154,6 +157,6 @@ class Game:
         if len(self.pacgums.sprites()) == 0 and not self.engine.loading:
             self.engine.loading = True
             self.engine.next_level()
-        if self.lives < 0:
+        if self.lives < 0 or self.timer.is_expired():
             # Le game over
             pass
--- src/hud.py
+++ src/hud.py
@@ -23,6 +23,13 @@ class HUD:
             self.arcade_font,
             (90, self.engine.screen_height - 15),
         )
+        self.timer = self.Timer(
+            self,
+            str(self.game.timer.current_time()),
+            "white",
+            self.arcade_font,
+            (160, self.engine.screen_height - 15),
+        )
 
     class Score(Text):
         def __init__(
@@ -54,4 +61,20 @@ class HUD:
 
         def update(self):
             self.text = str(self.hud.game.lives)
+            self.image = self.font.render(self.text, True, self.color)
+
+    class Timer(Text):
+        def __init__(
+            self,
+            hud: "HUD",
+            text: str,
+            color: str,
+            font: pygame.font.Font,
+            coordinates: tuple[int, int],
+        ):
+            super().__init__(text, color, font, coordinates)
+            self.hud = hud
+
+        def update(self):
+            self.text = str(self.hud.game.timer.current_time())
             self.image = self.font.render(self.text, True, self.color)
